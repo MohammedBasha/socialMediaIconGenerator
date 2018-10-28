cd5cd07802561a29ffecd7d83fcd7afe9093f73d
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-28 21:11:27 +0200

    adding an object containing the social icons

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var socialMedia = { // adding a list of property and value links
+  facebook : 'http://facebook.com/',
+  twitter: 'http://twitter.com/',
+  flickr: 'http://flickr.com/',
+  youtube: 'http://youtube.com/'
+};
